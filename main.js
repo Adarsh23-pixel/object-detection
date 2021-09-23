b33b21f1f4782cbdbdd7d999222dacc00cc1e58a
Adarsh23-pixel <76739763+Adarsh23-pixel@users.noreply.github.com>
2021-09-23 15:51:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+img = "";
+
+
+function preload (){
+img = loadImage('dog_cat.jpg');
+}
+
+
+
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    
+    }
+    
+    
+    function draw() {
+image(img,0,0,640,420);
+fill('red');
+text("dog",45,75);
+noFill()
+stroke("yellow");
+rect(30,60,400,350);
+
+}
+
